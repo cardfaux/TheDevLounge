fc8ceb6cdcdf8dc5b00342be7ce1882ed1365734
James Hagood <cardfaux@tutanota.com>
2020-01-30 19:50:47 -0600

    added Profile Model used both Profile Model and User Model To Start to make profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,60 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route   GET api/profile
-// @desc    Test Route
-// @access  Public
-router.get('/', (req, res) => res.send('Profile Route'));
+// Bring In Auth MiddleWare
+const auth = require('../../middleware/auth');
+
+// Bring In The Profile Model
+const Profile = require('../../models/Profile');
+// Bring In The User Model
+const User = require('../../models/User');
+
+// @route   GET api/profile/me
+// @desc    Get Current Users Profile
+// @access  Private
+router.get('/me', auth, async (req, res) => {
+	try {
+		// Get Name and Avatar Properties From The User Model and Add Them To The Profile Assign Them To A Constant
+		const profile = await Profile.findOne({
+			user: req.user.id
+		}).populate('user', ['name', 'avatar']);
+
+		// If No Profile Send Error Message
+		if (!profile) {
+			return res.status(400).json({ msg: 'There Is No Profile For This User' });
+		}
+
+		// If There Is A Profile Send The Profile From The Constant Declaration From Above
+		res.jason(profile);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
+
+// @route   POST api/profile/
+// @desc    Create or Update A User Profile
+// @access  Private
+router.post(
+	'/',
+	[
+		auth,
+		[
+			check('status', 'Status Is Required')
+				.not()
+				.isEmpty(),
+			check('skills', 'Skills Is Required')
+				.not()
+				.isEmpty()
+		]
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+	}
+);
 
 module.exports = router;
