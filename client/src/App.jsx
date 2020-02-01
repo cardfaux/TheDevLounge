5748ef3f089ceb091d6788e305ce2b1fd3b7df82
James Hagood <cardfaux@tutanota.com>
2020-02-01 16:26:57 -0600

    added a PrivateRoute Component and added the dashboard as a PrivateRoute so a user has to be logged in to access it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,22 @@
+// REACT Imports
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// REDUX
+// REDUX Imports
 import { Provider } from 'react-redux';
 import store from './store';
 import setAuthToken from './utils/setAuthToken';
 import { loadUser } from './actions/auth';
 
+// Component Imports
 import NavBar from './components/layout/NavBar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 
+// Gloal CSS Import
 import './App.css';
 
 if (localStorage.token) {
@@ -34,6 +39,7 @@ const App = () => {
 						<Switch>
 							<Route exact path='/register' component={Register} />
 							<Route exact path='/login' component={Login} />
+							<PrivateRoute exact path='/dashboard' component={Dashboard} />
 						</Switch>
 					</section>
 				</Fragment>
