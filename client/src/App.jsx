de8e3f6a763d92407ea90135f58e7c1329cd41b7
James Hagood <cardfaux@tutanota.com>
2020-02-01 14:00:18 -0600

    added loadUser action ClearUser Reducer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,28 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // REDUX
 import { Provider } from 'react-redux';
 import store from './store';
+import setAuthToken from './utils/setAuthToken';
+import { loadUser } from './actions/auth';
 
 import NavBar from './components/layout/NavBar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+
 import './App.css';
 
+if (localStorage.token) {
+	setAuthToken(localStorage.token);
+}
+
 const App = () => {
+	useEffect(() => {
+		store.dispatch(loadUser());
+	}, []);
+
 	return (
 		<Provider store={store}>
 			<Router>
